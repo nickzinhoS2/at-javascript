4a51ade7fd34a8e01fff0cde6e84b2465a53fa77
nickzinhoS2 <102757773+nickzinhoS2@users.noreply.github.com>
2022-05-04 16:50:33 -0300

    Add files via upload

---
+++ 1-exercicios.js
@@ -0,0 +1,24 @@
+//1
+console.log(10 + 20 * 30);
+//1-b
+console.log( 4**2 / 30);
+//1-c
+console.log((9**4 + 2)* 6 -1);
+
+//2
+console.log(10 % 3  * 10**1 + 1 - 10 *4 / 2);
+
+//3-a
+const nome = 'Nicolas';
+console .log(nome);
+//3-b
+console.log(2 * 3 * 3 *5);
+//3-c
+const a = 5;
+const b = 40;
+const c =70;
+const a = 5; .log(a+b+c);
+
+//3-d
+const a = 2;
+console.log(1800*a);
